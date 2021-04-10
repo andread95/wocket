da228cfdf7cbb32030a787a4647b731d38a014f3
andread95 <dangio_95@icloud.com>
2021-04-10 13:33:52 +0200

    test

--- server.js
+++ server.js
@@ -10,8 +10,13 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+var options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/live.demetralife.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/live.demetralife.com/fullchain.pem'),
+};
+
 app.prepare().then(() => {
-  const server = createServer((req, res) => {
+  const server = createServer(options, (req, res) => {
     const parsedUrl = parse(req.url, true);
     const { pathname, query } = parsedUrl;
 
