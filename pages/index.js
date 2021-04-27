290ebbf2fe404462a60aa525c4242aaaf9d65d84
andread95 <dangio_95@icloud.com>
2021-04-27 19:31:09 +0200

    test

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,50 @@ import Head from 'next/head';
 
 import 'bootstrap';
 
+function Home() {
+  const {data, revalidate} = useSWR('/api/me', async function(args) {
+    const res = await fetch(args);
+    return res.json();
+  });
+  if (!data) return <h1>Loading...</h1>;
+  let loggedIn = false;
+  if (data.email) {
+    loggedIn = true;
+  }
 
-export default () => {
-  
 
   return (
-    
+    <div>
+    <Head>
+      <title>Demetra Life</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <h1>Inizia ora la tua Diretta su Demetra Life</h1>
+
+    <h2>Inserisci le credenziali per accedere.</h2>
+    {loggedIn && (
+      <>
+        <p>Welcome {data.email}!</p>
+        <button
+          onClick={() => {
+            cookie.remove('token');
+            revalidate();
+          }}>
+          Logout
+        </button>
+      </>
+    )}
+    {!loggedIn && (
+      <>
+        <Link href="/login">Login</Link>
+        <p>or</p>
+        <Link href="/signup">Sign Up</Link>
+      </>
+    )}
+  </div>
     
   );
-};
+
+    }
+
+export default Home;
